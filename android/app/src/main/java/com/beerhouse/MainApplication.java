c169d933d995e7511a0b3aa8de82f947132e580e
samriddhis <samriddhis@github.com>
2020-01-21 23:13:53 +0530

    latest

--- android/app/src/main/java/com/beerhouse/MainApplication.java
+++ android/app/src/main/java/com/beerhouse/MainApplication.java
@@ -5,9 +5,10 @@ import android.util.Log;
 
 import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+//import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -28,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+     // packages.add(new RNFirebasePackage()); 
+      packages.add(new RNFirebaseMessagingPackage());
       return packages;
     }
 
